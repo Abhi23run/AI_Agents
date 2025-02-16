70999248c7b4dc05dd0dec4e751ccb7d255cac20
Abhinav Arun <abhinavarun23@gmail.com>
2025-02-16 14:37:44 -0500

    removing all the subfolder files

--- app.py
+++
@@ -1,8 +0,0 @@
-import gradio as gr
-from agent import run_conversation
-
-def gradio_interface(user_prompt):
-    return run_conversation(user_prompt)  
-
-interface = gr.Interface(fn=gradio_interface, inputs="text", outputs="text")
-interface.launch()
